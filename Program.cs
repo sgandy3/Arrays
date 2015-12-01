4ec1b5f1a9458c1c9fb43d247de29382fee5a375
Your Name <you@example.com>
2015-12-01 13:44:17 -0500

    Printing Arrays

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayname0916
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declare and initialize an array of characters spelling out your last name.
+            //Print the length of the array. 
+
+
+
+            char[] lastName = {'g','a','n','d','y'};
+            
+            Console.WriteLine(lastName.Length);
+
+            //Declare and initialize a string array. Using the Length property, 
+            //print the second to last element in the array.
+
+            Console.WriteLine(lastName[(lastName.Length) -2]);
+        }
+    }
+}
