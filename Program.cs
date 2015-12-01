7f7126e0666d8f021f12d51f69d551ec8e60db06
Your Name <you@example.com>
2015-12-01 14:11:25 -0500

    update

--- Program.cs
+++ Program.cs
@@ -13,16 +13,28 @@ namespace arrayname0916
             //Declare and initialize an array of characters spelling out your last name.
             //Print the length of the array. 
 
+          
 
+            char[] lastName = new char[5];
+            lastName[0] = 'g';
+            lastName[1] = 'a';
+            lastName[2] = 'n';
+            lastName[3] = 'd';
+            lastName[4] = 'y';
 
-            char[] lastName = {'g','a','n','d','y'};
-            
             Console.WriteLine(lastName.Length);
 
             //Declare and initialize a string array. Using the Length property, 
             //print the second to last element in the array.
 
-            Console.WriteLine(lastName[(lastName.Length) -2]);
+            string[] myLastName = new string [5];
+            myLastName[0] = "g";
+            myLastName[1] = "a";
+            myLastName[2] = "n";
+            myLastName[3] = "d";
+            myLastName[4] = "y";
+
+            Console.WriteLine(myLastName[(myLastName.Length) -2]);
         }
     }
 }
